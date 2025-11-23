ee97936049de1e42263791f54ca4899d84740b18
Renato Oliveira <renatog.oliveira17@gmail.com>
2025-11-23 19:06:03 -0300

    cors

--- src/main/java/com/renato/projects/appointment/security/config/CorsConfig.java
+++ src/main/java/com/renato/projects/appointment/security/config/CorsConfig.java
@@ -19,7 +19,9 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins(allowedOrigin)
+                    //.allowedOrigins(allowedOrigin)
+                .allowedOrigins("http://www.zendaavip.com.br", "http://zendaavip.com.br", "https://www.zendaavip.com.br", "https://zendaavip.com.br")
+                
                     //verificar se utilizando o localhost:3000, funciona em produção tbm
                 	//.allowedOrigins("http://localhost:3000")
                     .allowedMethods("PATCH","GET", "POST", "PUT", "DELETE", "OPTIONS")
